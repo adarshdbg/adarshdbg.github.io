ea49816f80c42a10c50c4f8738a90bf1f3e05f6b
adarshdbg <46560413+adarshdbg@users.noreply.github.com>
2019-11-27 14:01:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -280,15 +280,3 @@ jQuery(document).ready(function($) {
 	});
 
 });
-/*jslint plusplus: true */
-/*global $, jQuery, console, alert, prompt */
-
-$('.carousel').carousel();
-
-// Em An
-// 13-7-2017
-// https://codepen.io/collection/AGNJNB/
-// https://codepen.io/Dr_rakcha/pen/WOWrxr
-// https://codepen.io/Em-An/pen/NbQZjZ
-
-// https://codepen.io/mephysto/pen/ZYVKRY
